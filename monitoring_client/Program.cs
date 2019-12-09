43ac75f28a1515699168f23c029b4b5103b6bf1c
Zhangzhuangjie <spinechoo@gmail.com>
2019-12-09 21:47:52 +0800

    client init

---
+++ monitoring_client/Program.cs
@@ -0,0 +1,40 @@
+﻿using monitoring_client.utils;
+using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace monitoring_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var networks = NetworkInterface.GetAllNetworkInterfaces();
+            long lastBytesRec = 0;
+            long lastBytesSend = 0;
+            while (true)
+            {
+                long nowBytesRec = 0;
+                long nowBytesSend = 0;
+                foreach (var network in networks)
+                {
+                    nowBytesRec += network.GetIPv4Statistics().BytesReceived;
+                    nowBytesSend += network.GetIPv4Statistics().BytesSent;
+                }
+                var recinterval = nowBytesRec - lastBytesRec;
+                var sendinterval = nowBytesSend - lastBytesSend;
+                Console.WriteLine($"下载速度速度:{SystemHelper.getSpeed(recinterval, 2000)}");
+                Console.WriteLine($"上传速度速度:{SystemHelper.getSpeed(sendinterval, 2000)}");
+                Console.WriteLine($"下载总量:{SystemHelper.getSpeed(nowBytesRec)}");
+                Console.WriteLine($"上传总量:{SystemHelper.getSpeed(nowBytesSend)}");
+                lastBytesRec = nowBytesRec;
+                lastBytesSend = nowBytesSend;
+                Thread.Sleep(1000);
+            }
+        }
+
+
+    }
+}
---
+++ monitoring_client/utils/SystemHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace monitoring_client.utils
+{
+    public class SystemHelper
+    {
+        public static string getSpeed(long byteCount, int timestamp = 1000)
+        {
+            return getNumber(byteCount, timestamp) + getUnit(byteCount, timestamp);
+        }
+        private static string getNumber(long byteCount, int timestamp)
+        {
+            if (byteCount <= 1024)
+            {
+                return byteCount.ToString();
+            }
+            if (byteCount <= 1024 * 1024)
+            {
+                return Math.Round((byteCount / (1024.0)), 2).ToString();
+            }
+            return Math.Round((byteCount / (1024 * 1024.0)), 2).ToString();
+        }
+        private static string getUnit(long byteCount, int timestamp)
+        {
+            if (byteCount <= 1024)
+            {
+                return "b/s";
+            }
+            if (byteCount <= 1024 * 1024)
+            {
+                return "kb/s";
+            }
+            return "mb/s";
+        }
+    }
+}
