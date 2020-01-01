03894cd865c27f598bd172d89772767fa9fa13dd
Zhangzhuangjie <spinechoo@gmail.com>
2020-01-01 14:05:12 +0800

    add sln

--- monitoring_client/Program.cs
+++ monitoring_client/Program.cs
@@ -2,14 +2,35 @@
 using System;
 using System.Net.NetworkInformation;
 using System.Threading;
+using System.Text.Json;
+using monitoring_client.model;
 
 namespace monitoring_client
 {
     class Program
     {
+        static string url;
+        static int interval = 4000;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 1000;
+            int b = 234;
+            var c = a / b;
+
+            var sss = SystemHelper.ConvertByte(12345);
+            var s1 = sss.Item1;
+            var s2 = sss.Item2;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("请输入服务器地址:");
+                url = Console.ReadLine();
+            }
+            else
+            {
+                url = args[0];
+            }
+            //url = "http://imghosting.dcces.com/api/upload?inajax=1&ssl=1";
 
             var networks = NetworkInterface.GetAllNetworkInterfaces();
             long lastBytesRec = 0;
@@ -18,20 +39,31 @@ namespace monitoring_client
             {
                 long nowBytesRec = 0;
                 long nowBytesSend = 0;
-                foreach (var network in networks)
+                nowBytesSend = SystemHelper.getSendCount();
+                nowBytesRec = SystemHelper.getRecCount();
+                if (lastBytesRec == 0)
                 {
-                    nowBytesRec += network.GetIPv4Statistics().BytesReceived;
-                    nowBytesSend += network.GetIPv4Statistics().BytesSent;
+                    lastBytesRec = nowBytesRec;
+                    lastBytesSend = nowBytesSend;
                 }
                 var recinterval = nowBytesRec - lastBytesRec;
                 var sendinterval = nowBytesSend - lastBytesSend;
-                Console.WriteLine($"下载速度速度:{SystemHelper.getSpeed(recinterval, 2000)}");
-                Console.WriteLine($"上传速度速度:{SystemHelper.getSpeed(sendinterval, 2000)}");
-                Console.WriteLine($"下载总量:{SystemHelper.getSpeed(nowBytesRec)}");
-                Console.WriteLine($"上传总量:{SystemHelper.getSpeed(nowBytesSend)}");
+                Console.WriteLine($"下载速度速度:{SystemHelper.getSpeed(recinterval, interval)}" +
+                    $"上传速度速度:{SystemHelper.getSpeed(sendinterval, interval)}" +
+                    $"下载总量:{SystemHelper.getCount(nowBytesRec)}" +
+                    $"上传总量:{SystemHelper.getCount(nowBytesSend)}" +
+                    $"内存使用量:{SystemHelper.getMemory()}");
+                var model = new TranModel()
+                {
+                    RecSpeed = SystemHelper.getSpeed(recinterval, interval),
+                    SendSpeed = SystemHelper.getSpeed(sendinterval, interval),
+                    RecCount = SystemHelper.getCount(nowBytesRec),
+                    SendCount = SystemHelper.getCount(nowBytesSend)
+                };
+                new WebHelper().doPost(url, JsonSerializer.Serialize(model));
                 lastBytesRec = nowBytesRec;
                 lastBytesSend = nowBytesSend;
-                Thread.Sleep(1000);
+                Thread.Sleep(interval);
             }
         }
 
--- monitoring_client/utils/SystemHelper.cs
+++ monitoring_client/utils/SystemHelper.cs
@@ -1,38 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.NetworkInformation;
 using System.Text;
 
 namespace monitoring_client.utils
 {
     public class SystemHelper
     {
+        /// <summary>
+        /// 流量总计
+        /// </summary>
+        /// <param name="byteCount"></param>
+        /// <param name="timestamp"></param>
+        /// <returns></returns>
+        public static string getCount(long byteCount, int timestamp = 1000)
+        {
+            var tuple = ConvertByte(byteCount);
+            return $"{(tuple.Item1)}{ tuple.Item2}";
+        }
+        /// <summary>
+        /// 流量速度
+        /// </summary>
+        /// <param name="byteCount"></param>
+        /// <param name="timestamp"></param>
+        /// <returns></returns>
         public static string getSpeed(long byteCount, int timestamp = 1000)
         {
-            return getNumber(byteCount, timestamp) + getUnit(byteCount, timestamp);
+            var tuple = ConvertByte(byteCount * 1000 / timestamp);
+            return $"{(tuple.Item1)}{ tuple.Item2}/s";
         }
-        private static string getNumber(long byteCount, int timestamp)
+        public static long getSendCount()
         {
-            if (byteCount <= 1024)
+            var networks = NetworkInterface.GetAllNetworkInterfaces();
+            long nowBytesSend = 0;
+            foreach (var network in networks)
             {
-                return byteCount.ToString();
+                nowBytesSend += network.GetIPv4Statistics().BytesSent;
             }
-            if (byteCount <= 1024 * 1024)
-            {
-                return Math.Round((byteCount / (1024.0)), 2).ToString();
-            }
-            return Math.Round((byteCount / (1024 * 1024.0)), 2).ToString();
+            return nowBytesSend;
         }
-        private static string getUnit(long byteCount, int timestamp)
+        public static long getRecCount()
         {
-            if (byteCount <= 1024)
+            var networks = NetworkInterface.GetAllNetworkInterfaces();
+            long nowBytesRec = 0;
+            foreach (var network in networks)
             {
-                return "b/s";
+                nowBytesRec += network.GetIPv4Statistics().BytesReceived;
             }
-            if (byteCount <= 1024 * 1024)
-            {
-                return "kb/s";
-            }
-            return "mb/s";
+            return nowBytesRec;
         }
+        /// <summary>
+        /// 获取内存信息
+        /// </summary>
+        /// <returns></returns>
+        public static string getMemory()
+        {
+            var memoryInfos = File.ReadAllLines(@"/proc/meminfo");
+
+            foreach (string i in memoryInfos)
+            {
+                Console.WriteLine(i.ToString() + "\r");
+            }
+            var memoryTotalInfo = memoryInfos[0];
+
+            var processes = Process.GetProcesses();
+            long memoryCount = 0;
+            foreach (var process in processes)
+            {
+                memoryCount += process.WorkingSet64;
+
+            }
+            var tuple = ConvertByte(memoryCount);
+            var s = Environment.WorkingSet;
+            return tuple.Item1 + tuple.Item2;
+
+        }
+
+        /// <summary>
+        /// 根据字节数 返回对应的数据
+        /// </summary>
+        /// <param name="byteCount"></param>
+        /// <returns></returns>
+        public static Tuple<double, string> ConvertByte(long byteCount)
+        {
+            string[] units = new string[] { "b", "kb", "mb", "gb", "tb", "pb" };
+            long mod = 1024;
+            int i = 0;
+            double value = byteCount;
+            while (value >= mod)
+            {
+                // byteCount /= mod * 1.0;
+                //value = value / (mod * 1.0);
+                value /= (mod * 1.0);
+                i++;
+            }
+            return new Tuple<double, string>(Math.Round(Convert.ToDouble(value), 2), units[i]);
+        }
+
+
     }
 }
